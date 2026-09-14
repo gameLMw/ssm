aab00d435fa7cea2f6b98fd4cffebe74b8bbd91f
LM <wycalxy@outlook.com>
2026-09-14 15:49:50 +0800

    feat(tx): 添加Spring Boot应用主类
    - 创建了Spring03txApplication主类
    - 配置了@SpringBootApplication注解
    - 实现了main方法启动Spring Boot应用

---
+++ spring03tx/src/main/java/io/github/gamelmw/tx/Spring03txApplication.java
@@ -0,0 +1,13 @@
+package io.github.gamelmw.tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03txApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring03txApplication.class, args);
+    }
+
+}
