a3729f0809f773a03ec90b629e4a2aafef30fdfd
LM <wycalxy@outlook.com>
2026-09-14 17:22:41 +0800

    refactor(tx): 重构项目包结构并添加数据库操作注释
    - 将包路径从 io.github.gamelmw.tx 重命名为 io.github.gamelmw.spring.tx
    - 添加数据库操作相关注释说明
    - 注释中包含 JDBC 配置和 JdbcTemplate 使用指导

--- spring03tx/src/main/java/io/github/gamelmw/spring/tx/Spring03txApplication.java
+++ spring03tx/src/main/java/io/github/gamelmw/spring/tx/Spring03txApplication.java
@@ -1,7 +1,14 @@
-package io.github.gamelmw.tx;
+package io.github.gamelmw.spring.tx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * 操作数据库
+ * 1.导入包：spring-boot-starter-jdbc,mysql-connector-java
+ * 2.配置数据连接信息：spring.datasource.*
+ * 3.使用JdbcTemplate操作数据库
+ * */
 
 @SpringBootApplication
 public class Spring03txApplication {
