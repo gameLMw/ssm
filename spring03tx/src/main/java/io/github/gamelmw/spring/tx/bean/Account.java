b92caa6af1ac5e6f170f262926049db76a3d66f2
LM <wycalxy@outlook.com>
2026-09-14 17:21:18 +0800

    feat(bean): 添加账户实体类
    - 创建了Account实体类
    - 定义了id、username、age和balance属性
    - 使用Lombok注解简化getter/setter方法
    - 集成BigDecimal类型用于余额字段

---
+++ spring03tx/src/main/java/io/github/gamelmw/spring/tx/bean/Account.java
@@ -0,0 +1,14 @@
+package io.github.gamelmw.spring.tx.bean;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Account {
+
+    private Integer id;
+    private String username;
+    private Integer age;
+    private BigDecimal balance;
+}
