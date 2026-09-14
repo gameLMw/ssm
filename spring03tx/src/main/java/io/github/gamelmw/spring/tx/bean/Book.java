ee02769e15a848615aca42be3cfad08940851900
LM <wycalxy@outlook.com>
2026-09-14 17:22:18 +0800

    feat(book): 添加图书实体类
    - 创建了Book实体类包含id、书名、价格和库存字段
    - 使用Lombok注解简化getter/setter方法
    - 定义了图书基本信息的数据结构

---
+++ spring03tx/src/main/java/io/github/gamelmw/spring/tx/bean/Book.java
@@ -0,0 +1,14 @@
+package io.github.gamelmw.spring.tx.bean;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Book {
+
+    private Integer id;
+    private String bookName;
+    private BigDecimal price;
+    private Integer stock;
+}
